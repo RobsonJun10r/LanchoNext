c5eb4a33125e62933235ab88f52aa4b04e2f5886
RobsonJun10r <robson.alves.junior2@gmail.com>
2024-10-17 17:58:15 -0300

    add Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,36 @@
-import Image from "next/image"
-export default function Home(){
-  return(
-    <div>
-      <h1>Olá, seja bem-vindo</h1>
+import Link from "next/link";
+import Image from "next/image";
+export default function Home() {
+  return (
+    <div className="containerPage">
       <div>
-      <Image
-       src="/public/hamburguer.jpg"
-       alt="teste"
-       width={300}
-       height={200}
-       quality={75}
-       style = {{maxWidth: '100%', height: 'auto' }}
-      />
+        <h1 className="home">DATALANCHES</h1>
+        <div className="card">
+          <Link href="/ListarLanches">
+            <Image
+              src="/images/hamburguer.png"
+              alt="Hamburguer"
+              width={600}
+              height={300}
+              quality={75}
+              style={{ maxWidth: "100%", height: "auto", cursor: 'pointer' }}
+            />
+          </Link>
+        </div>
+        
+        <div className="card">
+        <Link href="/ListarProdutos">
+          <Image
+            src="/images/produto.png"
+            alt="Produtos"
+            width={600}
+            height={300}
+            quality={75}
+            style={{ maxWidth: "100%", height: "auto", cursor: 'pointer' }}
+          />
+          </Link>
+        </div>
       </div>
     </div>
-  )
+  );
 }
